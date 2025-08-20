cee155c8d376f7149e0489e55c27e3df20ebbac6
DominicW-Kami <DominicW@metropolia.fi>
2025-08-20 09:04:09 +0300

    Modular1

--- mod1/v002.py
+++ mod1/v002.py
@@ -1,15 +1,27 @@
 aa = input('Anna Nimesi: ')
 print("Tervehdys, " + aa,"!")
 import math
-def laske():
+
+def laske_ympyran_pinta_ala():
 
     try:
-        sade1 = input('Mikä on ympyrän säde? ')
-        sade2 = float(sade1)
-        if sade2 < 0:
-            print("Säde ei voi olla negatiivinen luku!")
+
+        sade_str = input("Mikä on ympyrän säde? ")
+        sade = float(sade_str)
+
+
+        if sade < 0:
+            print("Säde ei voi olla negatiivinen.")
         else:
-           pinta_ala = math.pi * (sade2 ** 2)
-           print(f"Ympyrän pinta-ala on: {pinta_ala:.2f} kun säde on {sade2:.2f}")
+
+            pinta_ala = math.pi * (sade ** 2)
+
+
+            print(f"Ympyrän pinta-ala on: {pinta_ala:.2f}")
+
     except ValueError:
-           print("Virheellinen syöte.")
+
+        print("Virheellinen syöte. Ole hyvä ja syötä numero.")
+
+
+laske_ympyran_pinta_ala()
