940ff915f4c99f8f30269e68a1218beb5e6a0d2a
DominicW-Kami <DominicW@metropolia.fi>
2025-08-26 13:20:01 +0300

    Mod3 push

--- mod3/v003.py
+++ mod3/v003.py
@@ -1,4 +1,3 @@
-import math
 import random
 
 def kuhan_mitta():
@@ -20,3 +19,44 @@ def kuhan_mitta():
     except ValueError:
         print("Virheellinen syöte.")
 kuhan_mitta()
+hyttiluokka = input("Syötä hytinluokka (LUX, A, B, C,): ")
+if hyttiluokka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella. ")
+elif hyttiluokka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hyttiluokka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hyttiluokka == "C":
+    print ("C on ikkunaton hytti autokannen alapuolella.")
+else:
+    print("Virheellinen hyttiluokka")
+
+Sukupuoli = input("Syötä biologinen sukupuolesi (mies/nainen): ")
+while True:
+    try:
+        hemoglobiini = float(input("Syötä hemoglobiiniarvosi (g/l): "))
+        break
+    except ValueError:
+        print("Virheellinen syöte")
+if Sukupuoli == "mies":
+    if 134 <= hemoglobiini <= 195:
+        print("Hemoglobiiniarvosi on normaali")
+    elif hemoglobiini < 134:
+        print("Hemoglobiiniarvosi on alhainen")
+    elif hemoglobiini > 195:
+        print("Hemoglobiiniarvosi on korkea.")
+elif Sukupuoli == "nainen":
+    if 117 <= hemoglobiini <= 175:
+        print("Hemoglobiiniarvosi on normaali")
+    elif hemoglobiini < 117:
+        print("Hemoglobiiniarvosi on alhainen")
+    elif hemoglobiini > 175:
+        print("Hemoglobiiniarvosi on korkea.")
+else:
+    print("Virheellinen syöte")
+
+vuosi = int(input("Syötä vuosiluku: "))
+if (vuosi % 4 == 0 and vuosi % 100 != 0) or (vuosi % 400 == 0 ):
+    print(f"{vuosi} on karkausvuosi. ")
+else:
+    print(f"{vuosi} ei ole karkausvuosi.")
